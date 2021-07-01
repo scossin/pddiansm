112ec0b6a1fbb9730b9195a9cd3fe5acc79c4240
sebastien cossin <sebastien.cossin@u-bordeaux.fr>
2021-07-01 08:58:00 +0200

    WIP changing tests

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -1,5 +1,7 @@
 import os
+import pkgutil
 import unittest
+import importlib.resources as pkg_resources
 from typing import List
 
 import pydantic
@@ -8,6 +10,7 @@ from pydantic import ValidationError
 from pddiansm.interfaces.interfaces_input import PatientDrugs
 from pddiansm.interfaces.interfaces_output import APIoutput
 from pddiansm.interfaces.interfaces_pddi import PDDI, SubstanceThesaurus
+from pddiansm.thesaurus.ThesaurusFilesBuilder import ThesaurusFilesBuilder
 
 
 def get_path(filename: str) -> str:
@@ -16,9 +19,15 @@ def get_path(filename: str) -> str:
 
 
 class MyTestCase(unittest.TestCase):
+
+    def test_load_thesauri_files(self):
+        thesauri_files = ThesaurusFilesBuilder().thesauri_files()
+        self.assertEqual(len(thesauri_files), 1)
+
     def test_interface_pddi(self):
-        path = get_path("../pddiansm/thesaurus/thesaurus_092019.json")
-        pddis = pydantic.parse_file_as(List[PDDI], path)
+        filename = "data/thesauri/2019_09/Thesaurus_09_2019.json"
+        data = pkgutil.get_data("pddiansm", filename)
+        pddis = pydantic.parse_raw_as(List[PDDI], data)
         pddi0 = pddis[0]
         self.assertEqual(pddi0.main_drug, "ABATACEPT")
         self.assertEqual(pddi0.between_main_and_plus_drug, "")
@@ -30,11 +39,12 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(pddi0.description, "Association DECONSEILLEEMajoration de l’immunodépression.")
 
     def test_interface_substance(self):
-        path = get_path("../pddiansm/thesaurus/index_substances092019.json")
-        substances = pydantic.parse_file_as(List[SubstanceThesaurus], path)
+        filename = "data/thesauri/2019_09/index_des_substances_09_2019.json"
+        data = pkgutil.get_data("pddiansm", filename)
+        substances = pydantic.parse_raw_as(List[SubstanceThesaurus], data)
         substance1 = substances[1]
         self.assertEqual(substance1.substance, "abciximab (c 7e3b fab)")
-        self.assertEqual(substance1.drug_classes, ["antiagrégants plaquettaires"])
+        self.assertEqual(substance1.drug_classes, ["antiagrégants plaquettaires", "autres médicaments agissant sur l'hémostase"])
 
     def test_interface_drug(self):
         path = get_path("../pddiansm/interfaces/patient_drugs_tests.json")
