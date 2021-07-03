651e38886193628f69a3b5ca85290433bdea29b7
sebastien cossin <sebastien.cossin@u-bordeaux.fr>
2021-07-03 18:23:53 +0200

    change setup.py to add files in data

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+import os
+from typing import List
+
+# https://github.com/pypa/setuptools/issues/1806#issuecomment-678377209
+def find_files_in_folder(top: str) -> List[str]:
+    data_files = []
+    for root, _, files in os.walk(top):
+        folder = root.split(os.path.sep, 1)[-1] # remove 'top'
+        for f in files:
+            if os.path.sep in root:
+                f = os.path.join(folder, f)
+            data_files.append(f)
+    return data_files
 
 from setuptools import setup, find_packages
 
@@ -18,5 +31,6 @@ setup(
     url='https://github.com/scossin/pddiansm',
     license=license,
     packages=find_packages(exclude=('tests')),
-    package_data={'pddiansm': ['data/*.tsv', 'data/*.json']},
+    package_data={'pddiansm': find_files_in_folder('pddiansm/data')},
+    include_package_data=True
 )
