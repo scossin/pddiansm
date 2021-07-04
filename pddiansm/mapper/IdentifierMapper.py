2223f2c71ea9f2a695cb4db5ea1af6537292c6bc
sebastien cossin <sebastien.cossin@u-bordeaux.fr>
2021-07-04 19:31:46 +0200

    comment IdentifierMapper

--- pddiansm/mapper/IdentifierMapper.py
+++ pddiansm/mapper/IdentifierMapper.py
@@ -9,6 +9,12 @@ from pddiansm.thesaurus.ThesaurusEntriesCollapsed import ThesaurusEntriesCollaps
 
 class IdentifierMapper(IMapper):
     def __init__(self, string_mapper: StringMapper, substance_mapping: ISubstanceMapping):
+        """
+        Convert an identifier to a substance(s)/drug_class(es) ; return IThesaurusEntries
+        :param string_mapper: it maps a string to a list of substances/drug_classes.
+        The role of string_mapper is mainly to identify a substance and to retrieve its drug_classes
+        :param substance_mapping: converts an identifier to a list of strings (one to n mappings)
+        """
         self.string_mapper = string_mapper
         self.substance_mapping = substance_mapping
 
@@ -17,6 +23,7 @@ class IdentifierMapper(IMapper):
         return self.search_moc_by_identifier(string)
 
     def search_moc_by_identifier(self, identifier) -> IThesaurusEntries:
+        # an identifier can be mapped to multiple substances / drug_classes
         mocs = self.substance_mapping.get_substances_mapped(identifier)
         muliple_thesaurus_entries: List[IThesaurusEntries] = [self.string_mapper.search_moc(moc) for moc in mocs]
         thesaurus_entries_collapsed = self.__collapse_thesaurus_entries(muliple_thesaurus_entries)
